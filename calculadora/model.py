a49c4db059be8babf6681a2541779ac4bd67bedf
Juan Esteban Vallejo Hincapie <juanvallejo240@gmail.com>
2024-07-30 10:58:26 -0500

    the functions for the calculator

---
+++ calculadora/model.py
@@ -0,0 +1,13 @@
+class Calculator:
+
+    def add(self, a: float, b: float) -> float:
+        return a + b
+
+    def substract(self, a: float, b: float) -> float:
+        return a - b
+
+    def nultiply(self, a: float, b: float) -> float:
+        return a * b
+
+    def divition(self, a: float, b: float) -> float:
+        return a / b
